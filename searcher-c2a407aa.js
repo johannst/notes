f4680b5fd3ccb07b755a0e2856549f69e21bcd93
johannst <johannst@users.noreply.github.com>
2026-01-29 21:27:17 +0000

    deploy: 6df781afa14746f1520e442c44fcabc66e1782ff

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bce0e30b.js',
+                path_to_root + 'searchindex-d1fdea1c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- tools/nix/stdenv/flake.nix
+++ tools/nix/stdenv/flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; };
     in {
       # Enable support for 'nix fmt'.
       formatter.${system} = pkgs.nixfmt;
