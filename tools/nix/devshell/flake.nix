279f8e5b85d4d62bd9b1857f786343a97cad0b0b
johannst <johannst@users.noreply.github.com>
2026-01-29 23:00:56 +0000

    deploy: 8fb507e63d2d2d89fa9e88d47a617aadca89222f

--- tools/nix/devshell/flake.nix
+++ tools/nix/devshell/flake.nix
@@ -13,6 +13,10 @@
           pkgs.zig
           pkgs.zls
         ];
+        # https://github.com/NixOS/nixpkgs/issues/270415
+        shellHook = ''
+          unset ZIG_GLOBAL_CACHE_DIR
+        '';
       };
     };
 }
