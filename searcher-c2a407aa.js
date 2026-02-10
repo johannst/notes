fe2b6f9efe7886f17e4fcedb8dd84a28b924f5ca
johannst <johannst@users.noreply.github.com>
2026-02-10 21:12:50 +0000

    deploy: 798566b4f721d1006ef55c2605ef24b4c308c77e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6c7a077b.js',
+                path_to_root + 'searchindex-64306807.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
